8b270b4d67a990a9b2c3173e3bd9d047f4bd9d27
Sitong Liu <stoneliuicloud@icloud.com>
2022-11-09 19:15:40 -0500

    Flutter app initial

--- lib/md5.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:crypto/crypto.dart';
-
-str2md5(var s) {
-  var content = new Utf8Encoder().convert(s);
-  var digest = md5.convert(content);
-  return digest;
-}
